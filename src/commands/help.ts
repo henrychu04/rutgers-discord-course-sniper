cc639b7a43fc76488eb376303191915282553436
henrychu04 <henrychu04@outlook.com>
2021-04-20 10:44:49 -0400

    updated help embed

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -12,18 +12,19 @@ export default abstract class Help {
       .setDescription('!snipe commands')
       .addFields(
         { name: '!snipe', value: 'Current courses being monitored', inline: false },
-        { name: '!snipe add', value: 'Add a course', inline: false },
+        { name: '!add', value: 'Add a course', inline: false },
+        { name: '!remove', value: 'Remove a course being monitored', inline: false },
         {
-          name: '!snipe webhook <webhook link>',
+          name: '!webhook <webhook link> OR !webhook',
           value: 'Add a webhook to receive notifications of open courses',
           inline: false,
         },
-        { name: '!snipe remove', value: 'Remove a course being monitored', inline: false }
+        { name: '!webhook test', value: `Test the user's webhook link to receive notifications`, inline: false }
       )
       .setTimestamp();
 
     await message.channel.send(embed).then(() => {
-      console.log('!snipe help completed\n');
+      console.log('!help completed\n');
     });
   }
 }
